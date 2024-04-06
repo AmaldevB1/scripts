8eaafa9a1db6065b8f3a50fb4663582d7a8e9b73
AmaldevB1 <51470775+AmaldevB1@users.noreply.github.com>
2024-04-06 19:33:26 +0530

    Create deployHelmChart.sh

---
+++ deployHelmChart.sh
@@ -0,0 +1,17 @@
+#./deployHelmChart.sh helm-chart-1.2.tgz
+
+helmChartFile=$1
+chartName=$(echo "$helmChartFile" | sed -n "s/^.*[a-z]-\([0-9.-]*\).tgz/\1/p")
+chartVersion=$(echo "$helmChartFile" | sed -n "s/\([a-z-]*\)-[0-9].*/\1/p")
+
+currentChartVersion=$(./helm get metadata $chartName | grep "^VERSION" | awk '{print $2}')
+
+./helm get values --all -o yaml >> $chartName.yaml
+
+sed -i "s/$currentChartVersion/$chartVersion/" ./chartName.yaml
+
+read shouldUpgrade
+if [ "$shouldUpgrade" = "y" ];
+then
+./helm upgrade $chartName ./chartFile -f ./$chartName.yaml --version $chartVersion
+fi
